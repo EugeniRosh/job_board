a763d9e076855acec408038497c7edee16aafbc9
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-06-21 14:10:24 +0300

    убрал создание VacanciesStorage

--- src/job_board/manage.py
+++ src/job_board/manage.py
@@ -2,11 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from core.models import VacanciesStorage
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_board.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "job_board.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,6 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-vacancies = VacanciesStorage()
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
