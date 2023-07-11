ee5194e2da96490ba1dca2ee57c09a85c65d3cb1
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-07-11 16:41:04 +0300

    изменил forms

--- src/job_board/core/forms.py
+++ src/job_board/core/forms.py
@@ -1,26 +1,40 @@
+from core.models import (
+    Competencies,
+    EmploymentFormats,
+    Genders,
+    ResponseStatuses,
+    UserStatuses,
+)
 from django import forms
 
-STATUS_VACANCY = (
-    ("open to work", "open to work"),
-    ("open for proposals", "open for proposals"),
-    ("not open for proposals", "not open for proposals"),
-)
-
-STATUS_JOBRESPONSE = (
-    ("created", "created"),
-    ("reviewed", "reviewed"),
-    ("rejected", "rejected"),
-    ("accepted for work", "accepted for work"),
-)
+STATUS_JOBRESPONSE = [
+    (status.status, status.status) for status in ResponseStatuses.objects.all()
+]
+COMPETENCE = [
+    (competence.competence, competence.competence)
+    for competence in Competencies.objects.all()
+]
+USER_STATUS = [(status.status, status.status) for status in UserStatuses.objects.all()]
+GENDERS = [(gender.gender, gender.gender) for gender in Genders.objects.all()]
+EMPLOYMENT_FORMATS = [
+    (value.employment_format, value.employment_format)
+    for value in EmploymentFormats.objects.all()
+]
 
 
 class VacancyForm(forms.Form):
     title = forms.CharField(label='Job title', max_length=50)
+    description = forms.CharField(label="Description", widget=forms.Textarea)
     company = forms.CharField(label='Company')
-    competence = forms.CharField(label='Competence', max_length=50)
-    experience = forms.CharField(label='Experience', max_length=50)
+    work_format = forms.CharField(label="Work format", widget=forms.Textarea)
+    employment_formats = forms.ChoiceField(
+        label="Employment formats", choices=EMPLOYMENT_FORMATS
+    )
+    competence = forms.ChoiceField(label='Competence', choices=COMPETENCE)
     min_salary = forms.IntegerField(label='Max_salary', min_value=1)
     max_salary = forms.IntegerField(label='Max_salary', min_value=1)
+    tags = forms.CharField(label="Tags", widget=forms.Textarea)
+    countries = forms.CharField(label="Countries", widget=forms.Textarea)
 
 
 class CompanyForm(forms.Form):
@@ -34,6 +48,7 @@ class CompanyForm(forms.Form):
     instagram = forms.CharField(label='Instagram', required=False)
     web_site = forms.CharField(label='Web site')
     twitter = forms.CharField(label='Twitter', required=False)
+    business_lines = forms.CharField(label="Business lines", widget=forms.Textarea)
     min_staff = forms.IntegerField(label='Min_staff', min_value=1)
     max_staff = forms.IntegerField(label='Max_staff', min_value=1)
     country = forms.CharField(label='Country', max_length=100)
@@ -44,35 +59,37 @@ class CompanyForm(forms.Form):
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label='Name')
-    surname = forms.CharField(label='Surname', max_length=50)
-    age = forms.IntegerField(label='Age', min_value=0, required=False)
-    work_format = forms.CharField(label='Work format')
-    competence = forms.CharField(label='Competence')
-    design_format = forms.CharField(label='Design format')
-    status = forms.ChoiceField(label='Status', choices=STATUS_VACANCY)
-    work_experience = forms.IntegerField(
-        label='Work experience', min_value=0, required=False
-    )
-    resume = forms.CharField(label='Resume')
-    photo = forms.CharField(label='Photo')
+    name = forms.CharField(label="Name", max_length=50)
+    surname = forms.CharField(label="Surname", max_length=50)
+    age = forms.IntegerField(label="Age", min_value=0)
+    work_experience = forms.IntegerField(label="Work experience", min_value=0)
+    resume = forms.CharField(label="Resume")
+    photo = forms.CharField(label="Photo", required=False)
     experience_description = forms.CharField(
-        label='Experience description', max_length=200
+        label="Experience description", max_length=500, widget=forms.Textarea
     )
+    work_format = forms.CharField(label="Work format", widget=forms.Textarea)
+    competence = forms.ChoiceField(label="Competence", choices=COMPETENCE)
+    employment_formats = forms.ChoiceField(
+        label="Employment formats", choices=EMPLOYMENT_FORMATS
+    )
+    status = forms.ChoiceField(label="Status", choices=USER_STATUS)
     min_salary = forms.IntegerField(label='Min salary', min_value=0, required=False)
     max_salary = forms.IntegerField(label='Max salary', min_value=0, required=False)
-    email = forms.EmailField(label='Email', max_length=50)
-    phone = forms.CharField(label='Phone', max_length=20)
-    country = forms.CharField(label='Country')
-    city = forms.CharField(label='City')
-    linkedin = forms.CharField(label='Linkedin', required=False)
-    github = forms.CharField(label='Github', required=False)
-    password = forms.CharField(label='Password')
-    login = forms.CharField(label='Login', max_length=30)
-    gender = forms.CharField(label='Gender', max_length=20)
+    language = forms.CharField(label="Languages", max_length=500, widget=forms.Textarea)
+    tags = forms.CharField(label="Tags", max_length=500, widget=forms.Textarea)
+    email = forms.EmailField(label="Email")
+    phone = forms.CharField(label="Phone", max_length=20)
+    linkedin = forms.CharField(label="", max_length=150, required=False)
+    github = forms.CharField(label="", max_length=150, required=False)
+    password = forms.CharField(label="", max_length=150)
+    login = forms.CharField(label="", max_length=30)
+    country = forms.CharField(label="Country", max_length=20)
+    city = forms.CharField(label="City", max_length=20)
+    gender = forms.ChoiceField(label="Gender", choices=GENDERS)
 
 
-class JobResponseForm(forms.Form):
+class VacancyResponseForm(forms.Form):
     user = forms.CharField(label="User")
     accompanying_text = forms.CharField(
         label="Accompanying text", max_length=500, widget=forms.Textarea
