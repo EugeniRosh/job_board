80a7881878971e5ad1eed76bd0ddc7abaf2db327
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-02 11:55:36 +0300

    add logging

--- src/job_board/core/presentation/views/company.py
+++ src/job_board/core/presentation/views/company.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
 from core.bussiness_logic.dto import CompanyDTO
@@ -22,49 +23,68 @@ if TYPE_CHECKING:
     from django.http.response import HttpResponse
 
 
+logger = logging.getLogger(__name__)
+
+
 @require_http_methods(["GET", "POST"])
 def companies_core_controller(request: WSGIRequest) -> HttpResponse:
-    company_list = get_all_company()
-    context = {"title": "Companies", "company_list": company_list}
-    return render(request, "core/companies_core.html", context=context)
+    try:
+        company_list = get_all_company()
+        context = {"title": "Companies", "company_list": company_list}
+        logger.info("received all companies")
+        return render(request, "core/companies_core.html", context=context)
+
+    except Exception as err:
+        logger.critical(err)
 
 
 @require_http_methods(["GET", "POST"])
 def add_company_core_controller(request: WSGIRequest) -> HttpResponse:
-    form = CompanyForm()
+    try:
+        form = CompanyForm()
 
-    if request.POST:
-        form_company = CompanyForm(data=request.POST, files=request.FILES)
-        if form_company.is_valid():
-            company = converter_data_ftom_dto(
-                dto=CompanyDTO, data=form_company.cleaned_data
-            )
+        if request.POST:
+            form_company = CompanyForm(data=request.POST, files=request.FILES)
+            if form_company.is_valid():
+                company = converter_data_ftom_dto(
+                    dto=CompanyDTO, data=form_company.cleaned_data
+                )
 
-            try:
-                add_company(company=company)
-            except CreateUniqueError:
+                try:
+                    add_company(company=company)
+                    logger.info("add new company")
+                except CreateUniqueError:
+                    form = form_company
+                    form.add_error("name", "Company already exists")
+            else:
                 form = form_company
-                form.add_error("name", "Company already exists")
-        else:
-            form = form_company
 
-    context = {"title": "Add Company", "form": form}
-    return render(request, "core/add_company_core.html", context=context)
+        context = {"title": "Add Company", "form": form}
+        return render(request, "core/add_company_core.html", context=context)
+
+    except Exception as err:
+        logger.critical(err)
 
 
 @require_http_methods(["GET"])
 def get_company_core_controller(request: WSGIRequest, company_id: int) -> HttpResponse:
     try:
-        company, business_lines = get_company_by_id(company_id=company_id)
-    except ValueNotExists:
-        return HttpResponseRedirect(redirect_to=reverse("companies_core"))
+        try:
+            company, business_lines = get_company_by_id(company_id=company_id)
+            logger.info(f"get company: id = {company_id}")
+        except ValueNotExists:
+            logger.error(f"non-existent company requested id = {company_id}")
+            return HttpResponseRedirect(redirect_to=reverse("companies_core"))
 
-    feedback = get_company_review(company_id=company_id)
+        feedback = get_company_review(company_id=company_id)
 
-    context = {
-        "title": "Company",
-        "company": company,
-        "feedback": feedback,
-        "business_lines": business_lines,
-    }
-    return render(request, "core/get_company_core.html", context=context)
+        context = {
+            "title": "Company",
+            "company": company,
+            "feedback": feedback,
+            "business_lines": business_lines,
+        }
+        return render(request, "core/get_company_core.html", context=context)
+
+    except Exception as err:
+        logger.critical(err)
