39132b40ddc23dad7cbe33b6f4c740e89f8dd0a8
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-02 11:55:53 +0300

    add logging

--- src/job_board/core/presentation/views/employee.py
+++ src/job_board/core/presentation/views/employee.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
 from core.bussiness_logic.dto import EmployeeDTO
@@ -14,27 +15,36 @@ if TYPE_CHECKING:
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
-require_http_methods(["GET", "POST"])
+logger = logging.getLogger(__name__)
 
 
+@require_http_methods(["GET", "POST"])
 def add_employee_core_controller(request: WSGIRequest, company_id: int) -> HttpResponse:
-    if request.POST:
-        form_employee = EmployeesForm(data=request.POST, files=request.FILES)
-        if form_employee.is_valid():
-            employee = from_dict(EmployeeDTO, form_employee.cleaned_data)
-            add_employee(data=employee, company_id=company_id)
+    try:
+        if request.POST:
+            form_employee = EmployeesForm(data=request.POST, files=request.FILES)
+            if form_employee.is_valid():
+                employee = from_dict(EmployeeDTO, form_employee.cleaned_data)
+                add_employee(data=employee, company_id=company_id)
+                logger.info("add new employee")
 
-    form = EmployeesForm()
-    context = {"title": "Add employee", "form": form}
-    return render(request, "core/add_employee_core.html", context=context)
+        form = EmployeesForm()
+        context = {"title": "Add employee", "form": form}
+        return render(request, "core/add_employee_core.html", context=context)
+
+    except Exception as err:
+        logger.critical(err)
 
 
-require_http_methods(["GET"])
-
-
+@require_http_methods(["GET"])
 def employees_company_core_controller(
     request: WSGIRequest, companyname: str
 ) -> HttpResponse:
-    employees = get_employees_company(company=companyname)
-    context = {"title": "Employees company", "employees": employees}
-    return render(request, "core/employees_company.html", context=context)
+    try:
+        employees = get_employees_company(company=companyname)
+        logger.info("received by all employees of the company")
+        context = {"title": "Employees company", "employees": employees}
+        return render(request, "core/employees_company.html", context=context)
+
+    except Exception as err:
+        logger.critical(err)
