322694bb22a71e977d7189ce09947687eae02dd9
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-04 21:35:17 +0300

    new model companies

--- src/job_board/core/models/companies.py
+++ src/job_board/core/models/companies.py
@@ -1,20 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 from .base_model import BaseModel
 
 
-class Companies(BaseModel):
-    name = models.CharField(max_length=100, unique=True)
+class Companies(BaseModel, AbstractBaseUser):
+    company_name = models.CharField(max_length=100, unique=True, blank=True)
     address = models.OneToOneField(
-        to="Addresses", related_name="companies", on_delete=models.CASCADE
+        to="Addresses", related_name="companies", on_delete=models.CASCADE, null=True
     )
-    staff_number = models.ForeignKey(
-        to="StaffNumber", related_name="companies", on_delete=models.CASCADE
-    )
+    staff_number = models.PositiveIntegerField()
     founding_year = models.SmallIntegerField()
     logo = models.FileField(upload_to="company/logo/", blank=True)
     description = models.TextField()
-    email = models.EmailField()
     phone = models.CharField(max_length=20)
     linkedin = models.CharField(max_length=150, blank=True)
     instagram = models.CharField(max_length=150, blank=True)
@@ -22,6 +21,9 @@ class Companies(BaseModel):
     twitter = models.CharField(max_length=150, blank=True)
     business_lines = models.ManyToManyField(
         to="BusinessLines", through="CompaniesBusinessLines"
+    )
+    auth_user = models.OneToOneField(
+        to=get_user_model(), related_name="companies", on_delete=models.CASCADE
     )
 
     class Meta:
