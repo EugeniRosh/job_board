437a9e055ae7f84c2426712fbc5cda2a4ae3ce78
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-04 21:28:09 +0300

    изменил дто под новую модель

--- src/job_board/core/bussiness_logic/dto/user.py
+++ src/job_board/core/bussiness_logic/dto/user.py
@@ -5,29 +5,26 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 
 @dataclass
 class UserDTO:
-    user_id: int | None
-    name: str
-    surname: str
-    age: int
-    work_format: str
-    competence: str
-    design_format: str
-    status: str
-    work_experience: int
-    resume: InMemoryUploadedFile
-    photo: InMemoryUploadedFile
-    experience_description: str
-    min_salary: int
-    max_salary: int
-    email: str
-    phone: str
-    country: str
-    city: str
-    linkedin: str | None
-    github: str | None
-    password: str
-    login: str
-    gender: str
-    tags: str
-    language: str
-    language_skill: str
+    name: str | None
+    surname: str | None
+    login: str | None
+    age: int | None
+    work_format: str | None
+    competence: str | None
+    design_format: str | None
+    status: str | None
+    work_experience: int | None
+    resume: InMemoryUploadedFile | None
+    photo: InMemoryUploadedFile | None
+    experience_description: str | None
+    min_salary: int | None
+    max_salary: int | None
+    phone: str | None
+    country: str | None
+    city: str | None
+    linkedin: str | None | None
+    github: str | None | None
+    gender: str | None
+    tags: str | None
+    language: str | None
+    language_skill: str | None
