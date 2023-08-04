6289470bee9d5895aa7b56993b391154bfb1b051
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-04 21:40:33 +0300

    new EmployeesForm

--- src/job_board/core/presentation/forms/employees.py
+++ src/job_board/core/presentation/forms/employees.py
@@ -7,23 +7,27 @@ from django import forms
 
 
 class EmployeesForm(forms.Form):
-    name = forms.CharField(label="Name", max_length=50, strip=True)
-    surname = forms.CharField(label="Surname", max_length=50, strip=True)
+    name = forms.CharField(label="Name", max_length=50, strip=True, required=False)
+    surname = forms.CharField(
+        label="Surname", max_length=50, strip=True, required=False
+    )
     photo = forms.ImageField(
         label="Photo",
+        required=False,
         validators=[
             ValidateFileExtension(["jpeg", "jpg", "png"]),
             ValidatorFileSize(5_000_000),
         ],
     )
-    country = forms.CharField(label="Country", max_length=100, strip=True)
-    city = forms.CharField(label="City", max_length=100, strip=True)
-    email = forms.EmailField(label="Email")
+    country = forms.CharField(
+        label="Country", max_length=100, strip=True, required=False
+    )
+    city = forms.CharField(label="City", max_length=100, strip=True, required=False)
     phone = forms.CharField(label="Phone", max_length=20, strip=True)
-    password = forms.CharField(label="Password", max_length=150)
     position = forms.CharField(
         label="Positions",
         widget=forms.Textarea,
+        required=False,
         strip=True,
         validators=[ValidatorMaxNumberValue(max_count=4)],
     )
