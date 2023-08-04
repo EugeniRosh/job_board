a56a3cea71f5555bdfa66ff9c36647f6c1e48ff4
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-04 21:27:43 +0300

    изменил дто под новую модель

--- src/job_board/core/bussiness_logic/dto/company.py
+++ src/job_board/core/bussiness_logic/dto/company.py
@@ -5,20 +5,17 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 
 @dataclass
 class CompanyDTO:
-    company_id: int | None
-    name: str
-    founding_year: int
-    logo: InMemoryUploadedFile
-    description: str
-    email: str | None
+    company_name: str | None
+    founding_year: int | None
+    logo: InMemoryUploadedFile | None
+    description: str | None
     business_lines: str
     phone: str
     linkedin: str | None
     instagram: str | None
     web_site: str
     twitter: str | None
-    min_staff: int
-    max_staff: int
+    staff_number: int
     country: str
     city: str
     street: str
