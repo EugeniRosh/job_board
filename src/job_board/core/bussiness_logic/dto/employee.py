f403fc968994953b6a8b35b06a2d415f12f9cc10
YauheniRoshchanka <roshchenkoevgen@gmail.com>
2023-08-04 21:27:54 +0300

    изменил дто под новую модель

--- src/job_board/core/bussiness_logic/dto/employee.py
+++ src/job_board/core/bussiness_logic/dto/employee.py
@@ -5,12 +5,11 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 
 @dataclass
 class EmployeeDTO:
-    name: str
-    surname: str
-    photo: InMemoryUploadedFile
-    country: str
-    city: str
-    email: str
-    phone: str
-    password: str
-    position: str
+    name: str | None
+    surname: str | None
+    login: str | None
+    photo: InMemoryUploadedFile | None
+    country: str | None
+    city: str | None
+    phone: str | None
+    position: str | None
